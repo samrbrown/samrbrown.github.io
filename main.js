b985415633c5c6f89abb9ee21f7f930116104b61
samrbrown <samraymondbrown@googlemail.com>
2021-01-05 15:06:21 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+//modal one
+$('#exp-one').click(function(){
+   $('#modal-one').css('display', 'flex');
+})
+
+$('.exp-one-close').click(function(){
+    $('#modal-one').css('display', 'none');
+})
+//modal two
+$('#exp-two').click(function(){
+    $('#modal-two').css('display', 'flex');
+})
+
+$('.exp-two-close').click(function(){
+    $('#modal-two').css('display', 'none');
+})
+//modal three
+$('#exp-three').click(function(){
+    $('#modal-three').css('display', 'flex');
+});
+
+$('.exp-three-close').click(function(){
+    $('#modal-three').css('display', 'none');
+});
+//modal four
+$('#exp-four').click(function(){
+    $('#modal-four').css('display', 'flex');
+});
+
+$('.exp-four-close').click(function(){
+    $('#modal-four').css('display', 'none');
+})
